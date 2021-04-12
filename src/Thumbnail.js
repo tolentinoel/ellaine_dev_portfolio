e9acd130e071db4f726467449f90242045bebc94
tolentinoel <rckrleyn@gmail.com>
2021-04-11 21:29:50 -0700

    readjusting padding on CSS

--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -4,16 +4,20 @@ import './styles/Projects.css';
 function Thumbnail(props) {
   return (
     <div className="project">
-      <div className="project-image">
-        <a className="projectInfo" href={props.link} target= "_blank" alt="website for app" rel="noreferrer">
-          <img id="pic" src={props.image} alt="Project preview"/>
-          <div className="projectTitle">{props.title}</div>
-        </a>
-      </div>
-      <div>
-        <a className="projectInfo" href={props.githubRepo} target= "_blank" alt="website for repo" rel="noreferrer">Github</a>
-      </div>
+      <div className="projectImage">
 
+        <a href={props.link} target= "_blank" alt="website for app" rel="noreferrer">
+          <img id="pic" src={props.image} alt="Project preview"/>
+        </a>
+
+        <span className="projectTitle">
+            <h4>
+              <a className="projectInfo" href={props.link} target= "_blank" alt="website for app" rel="noreferrer"> {props.title}</a> | 
+              <a className="projectInfo" href={props.githubRepo} target= "_blank" alt="website for repo" rel="noreferrer"> Github</a>
+            </h4>
+        </span>
+
+      </div>
     </div>
   );
 }
