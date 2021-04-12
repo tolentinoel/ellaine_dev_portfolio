03ee1e041b2057cc6a0e82fa4f53013fd6483206
tolentinoel <rckrleyn@gmail.com>
2021-04-11 20:58:50 -0700

    change and add className

--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -5,13 +5,13 @@ function Thumbnail(props) {
   return (
     <div className="project">
       <div className="project-image">
-        <a href={props.link} target= "_blank" alt="website for app" rel="noreferrer">
+        <a className="projectInfo" href={props.link} target= "_blank" alt="website for app" rel="noreferrer">
           <img id="pic" src={props.image} alt="Project preview"/>
-          <div className="project-title">{props.title}</div>
+          <div className="projectTitle">{props.title}</div>
         </a>
       </div>
-      <div className="project-repo">
-        <a href={props.githubRepo} target= "_blank" alt="website for repo" rel="noreferrer">Github</a>
+      <div>
+        <a className="projectInfo" href={props.githubRepo} target= "_blank" alt="website for repo" rel="noreferrer">Github</a>
       </div>
 
     </div>
