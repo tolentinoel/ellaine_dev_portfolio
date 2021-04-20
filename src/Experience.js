7da7e77e48de20e7414c60a0b683d637a7834493
tolentinoel <rckrleyn@gmail.com>
2021-04-19 21:57:51 -0700

    edit react bootstrap tab

--- src/Experience.js
+++ src/Experience.js
@@ -13,10 +13,10 @@ class Experience extends React.Component{
       <div className="expDiv">
         <h1 className="expHeader">Experience</h1>
 
-
+        <div id="expInfo">
         <Tab.Container id="left-tabs-example" defaultActiveKey="first">
           <Row>
-            <Col sm={2}>
+            <Col sm={4}>
               <Nav variant="tabs" className="flex-column">
                 <Nav.Item>
                   <Nav.Link eventKey="first">Hilton</Nav.Link>
@@ -35,7 +35,8 @@ class Experience extends React.Component{
                 </Nav.Item>
               </Nav>
             </Col>
-            <Col sm={6}>
+           
+            <Col sm={8}>
               <Tab.Content>
                 <Tab.Pane eventKey="first">
                   <h4>Front Desk Agent</h4>
@@ -60,26 +61,8 @@ class Experience extends React.Component{
             </Col>
           </Row>
         </Tab.Container>
-        {/* <div className="tablist" id="company">
-          <button id="tab-0" role="tab" tabindex="0" aria-selected="true" aria-controls="panel-0"><span>Upstatement</span></button>
-
+  
         </div>
-
-        <div id="expInfo">
-          <div id="panel-0" role="tabpanel" tabindex="0" aria-labelledby="tab-0" aria-hidden="false">
-            <h3><span>Engineer</span><span class="company"> @ <a href="https://www.upstatement.com/" class="inline-link" rel="noreferrer" target="_blank">Upstatement</a></span></h3><p class="range">May 2018 - Present</p>
-            <div>
-              <ul>
-                <li>Write modern, performant, maintainable code for a diverse array of client and internal projects</li>
-                <li>Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Gatsby, React, Craft, WordPress, Prismic, and Netlify</li>
-                <li>Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis</li>
-              </ul>
-            </div>
-          </div>
-          <p>Front Desk Associate</p>
-        </div> */}
-
-
 
       </div>
     )
