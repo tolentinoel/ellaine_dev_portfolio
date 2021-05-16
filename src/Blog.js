b9ee5ee27a3ce772b97087483f0906751636c494
tolentinoel <rckrleyn@gmail.com>
2021-05-16 15:08:14 -0700

    added a new blog, refreshing Blog component

--- src/Blog.js
+++ src/Blog.js
@@ -30,27 +30,29 @@ class Blog extends Component {
             <h1 className="blogsHeader" id="blogfeed">Blogs</h1>
             <p className="blogP">I do also maintain a blog! Here are my few recent ones!</p>
             <div className="blog">
-                {this.state.blogs.slice(0,4).map(content => {
-                    return (
-                    <Card className="blogCard" border="outline-info" bg="dark">
-                        <Card.Img variant="top" src={content.cover_image} />
-                        <Card.Header><a href={content.url} target="_blank" rel="noreferrer"><h5>{content.title}</h5></a></Card.Header>
-                        <Card.Body >
-                            <Card.Text className="blogBody">
-                            {/* <Card.Subtitle className="mb-1 text-muted">{content.tags.replaceAll(','," ")}</Card.Subtitle> */}
-                            <div className="subtitle">
-                            {content.tag_list.map(tag => {
-                                return <Card.Subtitle className="mb-1 text-muted">#{tag}</Card.Subtitle>
-                            })}
-                            </div>
-                                {content.description}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+            {this.state.blogs.slice(0,4).map(content => {
+                return (
+                <Card className="blogCard" border="outline-info" bg="dark">
+                    <Card.Img variant="top" src={content.cover_image} />
+                    <Card.Header><a href={content.url} target="_blank" rel="noreferrer"><h5>{content.title}</h5></a></Card.Header>
+                    <Card.Body >
+                        <Card.Text className="blogBody">
+                        {/* 1ST WAY TO SHOW TAGS AS A STRING */}
+                        <Card.Subtitle className="mb-1 text-muted">{content.tags.replaceAll(','," ")}</Card.Subtitle> 
+                        {/* 2ND WAY IS TO ITERATE THROUGH THE ARRAY OF TAG_LIST */}
+                        <div className="subtitle">
+                        {content.tag_list.map(tag => {
+                            return <Card.Subtitle className="mb-1 text-muted">#{tag}</Card.Subtitle>
+                        })}
+                        </div>
+                        {content.description}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
 
-                    )
-                })
-                }
+                )
+            })
+            }
 
             </div>
             </>
