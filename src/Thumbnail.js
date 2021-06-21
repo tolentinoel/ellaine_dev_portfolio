539fc101e795d7aa450b9125997a48f56eba2f8d
tolentinoel <rckrleyn@gmail.com>
2021-06-21 15:10:12 -0700

    editing size of shields on projects

--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -13,15 +13,15 @@ class Thumbnail extends React.Component {
             )
         case 'ruby' :
           return (
-            <img key={lang} src="https://img.shields.io/badge/-Ruby%20on%20Rails-A42E2B?logo=ruby%20on%20rails&logoColor=white&style=flat" alt={lang} width="80" height="20"/>
+            <img key={lang} src="https://img.shields.io/badge/-Ruby%20on%20Rails-A42E2B?logo=ruby%20on%20rails&logoColor=white&style=flat" alt={lang} width="110" height="20"/>
             )
         case 'html' :
           return (
-            <img key={lang} src="https://img.shields.io/badge/-Html5-A86454?logo=html5&logoColor=white&style=flat" alt={lang} width="80" height="20"/>
+            <img key={lang} src="https://img.shields.io/badge/-Html5-A86454?logo=html5&logoColor=white&style=flat" alt={lang} width="70" height="20"/>
             )
         case 'css' :
           return (
-            <img key={lang} src="https://img.shields.io/badge/-CSS3-2E3084?logo=css3&logoColor=white&style=flat" alt={lang} width="80" height="20"/>
+            <img key={lang} src="https://img.shields.io/badge/-CSS3-2E3084?logo=css3&logoColor=white&style=flat" alt={lang} width="60" height="20"/>
             )
         case 'javaScript':
           return (
@@ -33,7 +33,7 @@ class Thumbnail extends React.Component {
             )
         case 'heroku':
           return (
-            <img key={lang} src ="https://img.shields.io/static/v1?label=Hosted&message=Heroku&color=005571" alt={lang} width="90" height="20"/>
+            <img key={lang} src ="https://img.shields.io/static/v1?label=Hosted&message=Heroku&color=005571" alt={lang} width="100" height="20"/>
             )
         case 'netlify':
           return (
@@ -45,7 +45,7 @@ class Thumbnail extends React.Component {
             )
         case 'sqlite':
           return (
-            <img key={lang} src="https://img.shields.io/badge/-sqlite-005571?logo=sqlite&logoColor=white&style=flat" alt={lang} width="80" height="20"/>
+            <img key={lang} src="https://img.shields.io/badge/-sqlite-005571?logo=sqlite&logoColor=white&style=flat" alt={lang} width="60" height="20"/>
             )
         case 'github':
           return (
@@ -53,15 +53,15 @@ class Thumbnail extends React.Component {
             )
         case 'cli':
           return (
-            <img key={lang} src="https://img.shields.io/badge/-CLI-005571?logo=windows%20terminal&logoColor=white&style=flat" alt={lang} width="80" height="20"/>
+            <img key={lang} src="https://img.shields.io/badge/-CLI-005571?logo=windows%20terminal&logoColor=white&style=flat" alt={lang} width="50" height="20"/>
             )
         case 'ticketMaster':
           return (
-            <img key={lang} src="https://img.shields.io/badge/-TicketMaster%20API-005571?logo=ticketmaster&logoColor=white&style=flat" alt={lang} width="80" height="20"/>
+            <img key={lang} src="https://img.shields.io/badge/-TicketMaster%20API-005571?logo=ticketmaster&logoColor=white&style=flat" alt={lang} width="120" height="20"/>
             )
         case 'reactRouter':
           return (
-            <img key={lang} src="https://img.shields.io/badge/-React%20Router-005571?logo=react%20router&logoColor=white&style=flat" alt={lang} width="80" height="20"/>
+            <img key={lang} src="https://img.shields.io/badge/-React%20Router-005571?logo=react%20router&logoColor=white&style=flat" alt={lang} width="100" height="20"/>
         )
         case 'fontawesome':
           return (
