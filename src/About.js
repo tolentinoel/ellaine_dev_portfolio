462bceb3b14b103056cec0990b597be00b8c3d59
tolentinoel <rckrleyn@gmail.com>
2021-12-07 16:31:50 -0800

    cleanup experience info text, adjusted size and fix issue with color

--- src/About.js
+++ src/About.js
@@ -15,23 +15,22 @@ render(){
 
           </div>
           <h3 className="aboutText">
-            I am Ellaine Tolentino. My pronouns are She/They. I am a member of the LGBTQIA+ community, a first generation Filipino and I love the Arts and Music. My discovery of Software Development began back in 2009 in a computer class fiddling with the theme of my Friendster & MySpace accounts. I didn't knew I was in the realm of HTML and CSS then but I enjoyed it to the point I even ventured on embedding music & videos on my page!
+            I am Ellaine Tolentino. My pronouns are She/They. I am a member of the LGBTQIA+ community, a first generation Filipino who also loves Arts and Music. My introduction to software development began in 2009, tinkering with the themes of my Friendster and MySpace accounts. I had no idea I was working with HTML and CSS at the time, but I enjoyed it so much that I experimented in embedding music and movies on my page! 
             <br/>
-            Fast-forward to today, and now I am a coding bootcamp graduate with a degree in Interior Design. I consider myself as a full-stack dev but Front-end Development is my favorite! I am looking forward in producing creative and interesting products/projects that will serve as a great contribution to the community. I am also trying to learn more of data structures and practice alot more algorithms!
+            Today, I am a coding bootcamp graduate with a degree in Interior Design and a background in hospitality. I consider myself a full-stack developer, but my favorite is front-end development! I am excited to create new and interesting products/projects that will make a significant contribution to the community. I'm also attempting to learn more about data structures and practice a variety of algorithms!
             <br/>
             <br/>
             Here is my tech stack as of the moment:
             <br/>
             <br/>
             <div id ="techBadges">
-
-              <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" width="100" height="28" alt="html"/>
-              <img src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white" width="80" height="28" alt="css"/>
+              <img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E" width="130" height="28" alt="javascript"/>
+              <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" width="100" height="28" alt="React"/>
               <img src="https://img.shields.io/badge/Ruby_on_Rails-CC0000?style=for-the-badge&logo=ruby-on-rails&logoColor=white" width="140" height="28" alt="ruby"/>
               <img src="https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white" width="130" height="28" alt="postgresql"/>
               <img src="https://img.shields.io/badge/SQLite-07405E?style=for-the-badge&logo=sqlite&logoColor=white" width="100" height="28" alt="sqlite"/>
-              <img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E" width="130" height="28" alt="javascript"/>
-              <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" width="100" height="28" alt="React"/>
+              <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" width="100" height="28" alt="html"/>
+              <img src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white" width="80" height="28" alt="css"/>
               <img src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" width="130" height="28" alt="bootstrap"/>
               <img src="https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white" width="130" height="28" alt="heroku"/>
               <img src="https://img.shields.io/badge/firebase-ffca28?style=for-the-badge&logo=firebase&logoColor=white" width="120" height="28" alt="firebase"/>
--- src/Experience.js
+++ src/Experience.js
@@ -21,82 +21,97 @@ class Experience extends React.Component{
             <Col sm={3.5} id="companyPills">
               <Nav variant="pills" className="flex-column">
                 <Nav.Item>
-                  <Nav.Link eventKey="first">Hilton <Card.Subtitle className="mb-2 text-light">San Jose,CA | Oct 2016 – March 2020</Card.Subtitle></Nav.Link>
+                  <Nav.Link eventKey="first">Freelance<Card.Subtitle className="mb-2 text-light">Remote, US | April 2021 – Present</Card.Subtitle></Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="second">Beverly Heritage <Card.Subtitle className="mb-2 text-light">Milpitas,CA | May 2015 - May 2016</Card.Subtitle></Nav.Link>
+                  <Nav.Link eventKey="second">Hilton <Card.Subtitle className="mb-2 text-light">San Jose,CA | Oct 2016 – March 2020</Card.Subtitle></Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="third">DoubleTree by Hilton<Card.Subtitle className="mb-2 text-light">Fremont,CA | Feb 2014 - May 2016</Card.Subtitle></Nav.Link>
+                  <Nav.Link eventKey="third">Beverly Heritage <Card.Subtitle className="mb-2 text-light">Milpitas,CA | May 2015 - May 2016</Card.Subtitle></Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="fourth">Freelance Construction<Card.Subtitle className="mb-2 text-light">Bay Area,CA | Sep 2014 - 2014</Card.Subtitle></Nav.Link>
+                  <Nav.Link eventKey="fourth">DoubleTree by Hilton<Card.Subtitle className="mb-2 text-light">Fremont,CA | Feb 2014 - May 2016</Card.Subtitle></Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="fifth">GrandSpace Furniture & Interiors <Card.Subtitle className="mb-2 text-light">Quezon City, Philippines | April 2012 - 2013</Card.Subtitle></Nav.Link>
+                  <Nav.Link eventKey="fifth">Freelance Construction<Card.Subtitle className="mb-2 text-light">Bay Area,CA | Sep 2014 - 2014</Card.Subtitle></Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="sixth">GrandSpace Furniture & Interiors <Card.Subtitle className="mb-2 text-light">Quezon City, Philippines | April 2012 - 2013</Card.Subtitle></Nav.Link>
                 </Nav.Item>
               </Nav>
             </Col>
 
             <Col lg="auto" id="expInfoText">
               <Tab.Content id="tab-content">
+
                 <Tab.Pane eventKey="first">
-                  <h4 className="expInfoHeader">Front Desk Agent</h4>
-                  <ListGroup variant="flush" id="expList">
-                    <ul>
-                      <li>Performs detailed operational procedures that cater to a high-volume diverse user group.</li>
-                      <li>Proficient in operating systems and brand loyalty programs.</li>
-                      <li>Contributes to company revenue by upselling additional facilities and services.</li>
-                      <li>Provides accurate, descriptive company information to clients including the hours of operation, facility features, tourist destinations, and all services or amenities available.</li>
+                  <h4 className="expInfoHeader">Web Developer</h4>
+                  <ListGroup variant="flush">
+                    <ul class="expList">
+                      <li> Collaborated with the client to develop the overall look and design of a website</li>
+                      <li> Built and redesigned pages using HTML, CSS, JavaScript (ES6), React.js framework, Bootstrap.</li>
+                      <li> Produced, maintained and modified websites and its user interface.</li>
+                      <li> Created interfaces that support various platforms, devices and screen resolution.</li>
                     </ul>
                   </ListGroup>
                 </Tab.Pane>
 
                 <Tab.Pane eventKey="second">
-                  <h4 className="expInfoHeader">Front Office Supervisor</h4>
+                  <h4 className="expInfoHeader">Front Desk Agent</h4>
                   <ListGroup variant="flush" >
-                    <ul>
-                      <li>Balances hotel occupancy and contributes to company revenue by upselling.</li>
-                      <li>Serves as manager on duty during the shift and implements standard operating procedures.</li>
-                      <li>Provides hands-on training & customer relationship management coaching to new associates.</li>
-                      <li>Monitors employee productivity, provides constructive feedback and coaching.</li>
-                      <li>Awarded as Employee of the Month in December 2015</li>
+                    <ul class="expList">
+                      <li>Performed detailed operational procedures that cater to a high-volume diverse user group.</li>
+                      <li>Proficient in operating systems and brand loyalty programs.</li>
+                      <li>Contributed to company revenue by upselling additional facilities and services.</li>
                     </ul>
                   </ListGroup>
                 </Tab.Pane>
 
                 <Tab.Pane eventKey="third">
+                  <h4 className="expInfoHeader">Front Office Supervisor</h4>
+                  <ListGroup variant="flush" >
+                    <ul class="expList">
+                      <li>Balanced hotel occupancy and contributes to company revenue by upselling.</li>
+                      <li>Served as manager on duty during the shift and implemented standard operating procedures.</li>
+                      <li>Provided hands-on training & customer relationship management coaching to new associates.</li>
+                      <li>Monitored employee productivity, provided constructive feedback and coaching.</li>
+                      <li>Awarded as Team member for the month of December 2015</li>
+                    </ul>
+                  </ListGroup>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="fourth">
                   <h4 className="expInfoHeader">Guest Service Agent</h4>
                   <ListGroup variant="flush" >
-                    <ul>
-                      <li>Satisfies and delights customers by fulfilling client needs or requests.</li>
-                      <li>Maintains property occupancy of 100% with 315 rooms and performs standard operating procedures. </li>
-                      <li>Handles general office duties and delivers customer service to a high-volume diverse user group or event.</li>
+                    <ul class="expList">
+                      <li>Maintained property occupancy of 100% with 315 rooms and performed standard operating procedures. </li>
+                      <li>Handled general office duties and delivered customer service to a high-volume diverse user group or event.</li>
                       <li>Awarded as Front Office Service Superstar in 2014</li>
                       <li>Awarded as November 2014's Team Member of the Month</li>
                     </ul>
                   </ListGroup>
                 </Tab.Pane>
 
-                <Tab.Pane eventKey="fourth">
+                <Tab.Pane eventKey="fifth">
                   <h4 className="expInfoHeader">Construction Intern</h4>
-                  <ListGroup variant="flush">
-                    <ul>
+                  <ListGroup variant="flush" >
+                    <ul class="expList">
+                      <li>Collaborated with general contractors and client to resolve design conflicts.</li>
                       <li>Provided detailed proposals to clients including but not limited to contracts, working, drawings, & color schemes.</li>
                       <li>Handled paperwork, payments and consultation between contractors, clients & suppliers.</li>
-                      <li>Proficient in operating tools and machinery in carpentry, masonry, and painting.</li>
+                      <li>Utilized various types of tools and machinery in carpentry, masonry, and painting.</li>
                     </ul>
                   </ListGroup>
                 </Tab.Pane>
 
-                <Tab.Pane eventKey="fifth">
+                <Tab.Pane eventKey="sixth">
                   <h4 className="expInfoHeader">Interior Designer</h4>
                   <ListGroup variant="flush" >
-                    <ul>
-                      <li>Prepares sketches, proposals, quotations, and contracts for the clients.</li>
-                      <li>Manages project planning, design execution, and revisions between contractors, clients & suppliers.</li>
-                      <li>Oversees the progress of project work and updates the project plans to reflect actual performance.</li>
-                      <li>Delivers highly detailed swatch board & 2D/3D CAD working drawings.</li>
+                    <ul class="expList">
+                      <li>Prepared sketches, proposals, quotations, and contracts for the clients.</li>
+                      <li>Managed project planning, design execution, and revisions between contractors, clients & suppliers.</li>
+                      <li>Oversee the progress of project work and update the project plans to reflect actual performance.</li>
+                      <li>Delivered highly detailed swatch board & 2D/3D CAD working drawings.</li>
                     </ul>
                   </ListGroup>
 
